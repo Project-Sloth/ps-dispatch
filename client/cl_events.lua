7b0d457a961f73ab4726996ef7f2d9e675f1c7c6
Hyper <rjfance@gmail.com>
2022-03-21 15:24:19 -0400

    Base QBCore Alerts

--- client/cl_events.lua
+++ client/cl_events.lua
@@ -105,3 +105,211 @@ RegisterNetEvent("qb-dispatch:client:fight", function()
         job = {"police"}
     })
 end)
+
+RegisterNetEvent("qb-dispatch:client:civdown", function()
+    local currentPos = GetEntityCoords(PlayerPedId())
+    local locationInfo = getStreetandZone(currentPos)
+    local gender = GetPedGender()
+    local heading = getCardinalDirectionFromHeading()
+    PlaySound(-1, "Lose_1st", "GTAO_FM_Events_Soundset", 0, 0, 1)
+    TriggerServerEvent("dispatch:server:notify",{
+        dispatchcodename = "civdown", -- has to match the codes in sv_dispatchcodes.lua so that it generates the right blip
+        dispatchCode = "10-69",
+        firstStreet = locationInfo,
+        gender = gender,
+        model = nil,
+        plate = nil,
+        priority = 2, -- priority
+        firstColor = nil,
+        automaticGunfire = false,
+        origin = {
+            x = currentPos.x,
+            y = currentPos.y,
+            z = currentPos.z
+        },
+        dispatchMessage = "Civilian Down", -- message
+        job = {"ambulance"} -- jobs that will get the alerts
+    })
+end)
+
+
+RegisterNetEvent("qb-dispatch:client:storerobbery", function(camId)
+    local currentPos = GetEntityCoords(PlayerPedId())
+    local locationInfo = getStreetandZone(currentPos)
+    local gender = GetPedGender()
+    local heading = getCardinalDirectionFromHeading()
+    PlaySound(-1, "Lose_1st", "GTAO_FM_Events_Soundset", 0, 0, 1)
+    TriggerServerEvent("dispatch:server:notify",{
+        dispatchcodename = "storerobbery", -- has to match the codes in sv_dispatchcodes.lua so that it generates the right blip
+        dispatchCode = "10-90",
+        firstStreet = locationInfo,
+        camId = camId,
+        model = nil,
+        plate = nil,
+        priority = 2, -- priority
+        firstColor = nil,
+        automaticGunfire = false,
+        origin = {
+            x = currentPos.x,
+            y = currentPos.y,
+            z = currentPos.z
+        },
+        dispatchMessage = "Store Robbery", -- message
+        job = {"police"} -- jobs that will get the alerts
+    })
+end)
+
+RegisterNetEvent("qb-dispatch:client:fleecabankrobbery", function()
+    local currentPos = GetEntityCoords(PlayerPedId())
+    local locationInfo = getStreetandZone(currentPos)
+    local gender = GetPedGender()
+    local heading = getCardinalDirectionFromHeading()
+    PlaySound(-1, "Lose_1st", "GTAO_FM_Events_Soundset", 0, 0, 1)
+    TriggerServerEvent("dispatch:server:notify",{
+        dispatchcodename = "bankrobbery", -- has to match the codes in sv_dispatchcodes.lua so that it generates the right blip
+        dispatchCode = "10-90",
+        firstStreet = locationInfo,
+        model = nil,
+        plate = nil,
+        priority = 2, -- priority
+        firstColor = nil,
+        automaticGunfire = false,
+        origin = {
+            x = currentPos.x,
+            y = currentPos.y,
+            z = currentPos.z
+        },
+        dispatchMessage = "Fleeca Bank Robbery", -- message
+        job = {"police"} -- jobs that will get the alerts
+    })
+end)
+
+RegisterNetEvent("qb-dispatch:client:paletobankrobbery", function()
+    local currentPos = GetEntityCoords(PlayerPedId())
+    local locationInfo = getStreetandZone(currentPos)
+    local gender = GetPedGender()
+    local heading = getCardinalDirectionFromHeading()
+    PlaySound(-1, "Lose_1st", "GTAO_FM_Events_Soundset", 0, 0, 1)
+    TriggerServerEvent("dispatch:server:notify",{
+        dispatchcodename = "paletobankrobbery", -- has to match the codes in sv_dispatchcodes.lua so that it generates the right blip
+        dispatchCode = "10-90",
+        firstStreet = locationInfo,
+        model = nil,
+        plate = nil,
+        priority = 2, -- priority
+        firstColor = nil,
+        automaticGunfire = false,
+        origin = {
+            x = currentPos.x,
+            y = currentPos.y,
+            z = currentPos.z
+        },
+        dispatchMessage = "Paleto Bank Robbery", -- message
+        job = {"police"} -- jobs that will get the alerts
+    })
+end)
+
+RegisterNetEvent("qb-dispatch:client:pacificbankrobbery", function()
+    local currentPos = GetEntityCoords(PlayerPedId())
+    local locationInfo = getStreetandZone(currentPos)
+    local gender = GetPedGender()
+    local heading = getCardinalDirectionFromHeading()
+    PlaySound(-1, "Lose_1st", "GTAO_FM_Events_Soundset", 0, 0, 1)
+    TriggerServerEvent("dispatch:server:notify",{
+        dispatchcodename = "pacificbankrobbery", -- has to match the codes in sv_dispatchcodes.lua so that it generates the right blip
+        dispatchCode = "10-90",
+        firstStreet = locationInfo,
+        model = nil,
+        plate = nil,
+        priority = 2, -- priority
+        firstColor = nil,
+        automaticGunfire = false,
+        origin = {
+            x = currentPos.x,
+            y = currentPos.y,
+            z = currentPos.z
+        },
+        dispatchMessage = "Pacific Bank Robbery", -- message
+        job = {"police"} -- jobs that will get the alerts
+    })
+end)
+
+RegisterNetEvent("qb-dispatch:client:vangelicorobbery", function()
+    local currentPos = GetEntityCoords(PlayerPedId())
+    local locationInfo = getStreetandZone(currentPos)
+    local gender = GetPedGender()
+    local heading = getCardinalDirectionFromHeading()
+    PlaySound(-1, "Lose_1st", "GTAO_FM_Events_Soundset", 0, 0, 1)
+    TriggerServerEvent("dispatch:server:notify",{
+        dispatchcodename = "vangelicorobbery", -- has to match the codes in sv_dispatchcodes.lua so that it generates the right blip
+        dispatchCode = "10-90",
+        firstStreet = locationInfo,
+        model = nil,
+        plate = nil,
+        priority = 2, -- priority
+        firstColor = nil,
+        automaticGunfire = false,
+        origin = {
+            x = currentPos.x,
+            y = currentPos.y,
+            z = currentPos.z
+        },
+        dispatchMessage = "Vangelico Robbery", -- message
+        job = {"police"} -- jobs that will get the alerts
+    })
+end)
+
+RegisterNetEvent("qb-dispatch:client:houserobbery", function()
+    local currentPos = GetEntityCoords(PlayerPedId())
+    local locationInfo = getStreetandZone(currentPos)
+    local gender = GetPedGender()
+    local heading = getCardinalDirectionFromHeading()
+    PlaySound(-1, "Lose_1st", "GTAO_FM_Events_Soundset", 0, 0, 1)
+    TriggerServerEvent("dispatch:server:notify",{
+        dispatchcodename = "houserobbery", -- has to match the codes in sv_dispatchcodes.lua so that it generates the right blip
+        dispatchCode = "10-90",
+        firstStreet = locationInfo,
+        model = nil,
+        plate = nil,
+        priority = 2, -- priority
+        firstColor = nil,
+        automaticGunfire = false,
+        origin = {
+            x = currentPos.x,
+            y = currentPos.y,
+            z = currentPos.z
+        },
+        dispatchMessage = "House Robbery", -- message
+        job = {"police"} -- jobs that will get the alerts
+    })
+end)
+
+RegisterNetEvent("qb-dispatch:client:drugsale", function()
+    local currentPos = GetEntityCoords(PlayerPedId())
+    local locationInfo = getStreetandZone(currentPos)
+    local gender = GetPedGender()
+    local heading = getCardinalDirectionFromHeading()
+    PlaySound(-1, "Lose_1st", "GTAO_FM_Events_Soundset", 0, 0, 1)
+    TriggerServerEvent("dispatch:server:notify",{
+        dispatchcodename = "suspicioushandoff", -- has to match the codes in sv_dispatchcodes.lua so that it generates the right blip
+        dispatchCode = "10-60",
+        firstStreet = locationInfo,
+        gender = gender,
+        model = nil,
+        plate = nil,
+        priority = 2, -- priority
+        firstColor = nil,
+        automaticGunfire = false,
+        origin = {
+            x = currentPos.x,
+            y = currentPos.y,
+            z = currentPos.z
+        },
+        dispatchMessage = "Suspicious Handoff", -- message
+        job = {"police"} -- jobs that will get the alerts
+    })
+end)
+
+RegisterCommand('test',function ()
+    TriggerEvent('qb-dispatch:client:drugsale')
+end)
