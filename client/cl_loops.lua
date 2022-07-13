d2091b46561019f34ddf22e776b315d76e7fb1d7
JericoFX <56100605+JericoFX@users.noreply.github.com>
2022-07-12 21:44:41 -0500

    Update cl_loops.lua

--- client/cl_loops.lua
+++ client/cl_loops.lua
@@ -1,62 +1,35 @@
-local vehicleWhitelist = { [0] = true, [1] = true, [2] = true, [3] = true, [4] = true, [5] = true, [6] = true, [7] = true, [8] = true, [9] = true, [10] = true, [11] = true, [12] = true, [17] = true, [19] = true, [20] = true }
+local vehicleWhitelist = { [0] = true, [1] = true, [2] = true, [3] = true, [4] = true, [5] = true, [6] = true, [7] = true,
+    [8] = true, [9] = true, [10] = true, [11] = true, [12] = true, [17] = true, [19] = true, [20] = true }
 
--- CreateThread(function()
---
---     local sleep = 100
---     while true do
---         playerPed = PlayerPedId()
---         if (not isPlayerWhitelisted or Config.Debug) then
---             for k, v in pairs(Config.Timer) do
---                 if v > 0 then Config.Timer[k] = v - 1 end
---             end
---             if GetVehiclePedIsUsing(playerPed) ~= 0 then
---                 local vehicle = GetVehiclePedIsUsing(playerPed, true)
---                 if vehicleWhitelist[GetVehicleClass(vehicle)] then
---                     local driver = GetPedInVehicleSeat(vehicle, -1)
---                     if Config.Timer['Shooting'] == 0 and not BlacklistedWeapon(playerPed) and not IsPedCurrentWeaponSilenced(playerPed) and IsPedArmed(playerPed, 4) then
---                         sleep = 10
---                         if IsPedShooting(playerPed) then
---                             local vehicle = vehicleData(vehicle)
---                             exports['ps-dispatch']:VehicleShooting(vehicle)
---                             Config.Timer['Shooting'] = Config.Shooting.Success
---                         else
---                             Config.Timer['Shooting'] = Config.Shooting.Fail
---                         end
---                     end
---                     --     sleep = 100
---                     --     if (GetEntitySpeed(vehicle) * 3.6) >= (120 + (math.random(30, 60))) then
---                     --         Wait(400)
---                     --         if IsPedInAnyVehicle(playerPed, true) and ((GetEntitySpeed(vehicle) * 3.6) >= 90) then
---                     --             local vehicle = vehicleData(vehicle)
---                     --             exports['ps-dispatch']:SpeedingVehicle(vehicle)
---                     --             Config.Timer['Speeding'] = Config.Speeding.Success
---                     --         end
---                     --     else
---                     --         Config.Timer['Speeding'] = Config.Speeding.Fail
---                     --     end
---                     -- else
---                     --     sleep = 100
---                     -- end
---                 end
---             else
---                 -- if Config.Timer['Shooting'] == 0 and not IsPedCurrentWeaponSilenced(playerPed) and IsPedArmed(playerPed, 4) then
---                 --     sleep = 50
---                 --     if IsPedShooting(playerPed) and not BlacklistedWeapon(playerPed) then
---                 --         exports['ps-dispatch']:Shooting()
---                 --         Config.Timer['Shooting'] = Config.Shooting.Success
---                 --     else
---                 --         Config.Timer['Shooting'] = Config.Shooting.Fail
---                 --     end
---                 -- elseif Config.Timer['Melee'] == 0 and IsPedInMeleeCombat(playerPed) and HasPedBeenDamagedByWeapon(GetMeleeTargetForPed(playerPed), 0, 1) then
---                 --     sleep = 50
---                 --     exports['ps-dispatch']:Fight()
---                 --     Config.Timer['Melee'] = Config.Melee.Success
---                 -- else sleep = 100 end
---             end
---         end
---         Wait(sleep)
---     end
--- end)
+local check = false
+---This event will run only when the player enter a vehicle and will stop inmediate after
+---@param ad any
+AddEventHandler("CEventShockingSeenCarStolen", function(ad)
+    check = true
+    local playerPed = PlayerPedId()
+    CreateThread(function()
+        while check do
+            Wait(200)
+            if IsPedInAnyVehicle(playerPed, true) then
+                local Vehicle = GetVehiclePedIsIn(playerPed, true)
+                if not Config.Reckless then
+                    if ((GetEntitySpeed(Vehicle) * 3.6) >= 90) then
+                        local vehicle = vehicleData(Vehicle)
+                        exports['ps-dispatch']:SpeedingVehicle(vehicle)
+                        Config.Reckless = true
+                        Citizen.SetTimeout(10000, function()
+                            Config.Reckless = false
+                        end)
+                    end
+                end
+            else
+                check = false
+            end
+        end
+    end)
+end)
+
+
 
 ---Event fired every time a player shoot, b arg is the player Ped
 ---@param _ table
@@ -68,7 +41,8 @@ AddEventHandler("CEventGunShot", function(_, b, c)
         if IsPedInAnyVehicle(Player) then
             local Vehicle = GetVehiclePedIsIn(Player, true)
             if vehicleWhitelist[GetVehicleClass(Vehicle)] then
-                if not Config.VehicleShooting and not BlacklistedWeapon(playerPed) and not IsPedCurrentWeaponSilenced(Player) and IsPedArmed(Player, 4) then
+                if not Config.VehicleShooting and not BlacklistedWeapon(playerPed) and
+                    not IsPedCurrentWeaponSilenced(Player) and IsPedArmed(Player, 4) then
                     local vehicle = vehicleData(Vehicle)
                     exports['ps-dispatch']:VehicleShooting(vehicle)
                     Config.VehicleShooting = true
@@ -107,22 +81,4 @@ AddEventHandler("CEventDamage", function(_, b, c)
         end
     end
 end)
----Need to check this event, this fire every time a driver is doing crazy stuff like jumping on street, going too fast doesn't trigger
----@param a table
----@param b number
----@param c nil
-AddEventHandler("CEventShockingMadDriver", function(a, b, c)
-    local Player = b
-    local Vehicle = GetVehiclePedIsIn(Player, true)
-    if not Config.Reckless then
-        if ((GetEntitySpeed(Vehicle) * 3.6) >= 90) then
-            print(((GetEntitySpeed(Vehicle) * 3.6) >= 90))
-            local vehicle = vehicleData(Vehicle)
-            exports['ps-dispatch']:SpeedingVehicle(vehicle)
-            Config.Reckless = true
-            Citizen.SetTimeout(10000, function()
-                Config.Reckless = false
-            end)
-        end
-    end
-end)
+
