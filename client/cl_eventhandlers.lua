a55f1c9ecfe088da1f9bd9567f20fefc75d9b555
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2023-01-21 08:54:30 +1100

    Linting and PoliceJob Check
    Added one line return if debug mode is off and the person who triggered the alert is a police officer.

--- client/cl_eventhandlers.lua
+++ client/cl_eventhandlers.lua
@@ -20,29 +20,30 @@ AddEventHandler("CEventGunShot", function(witnesses, ped)
     -- Else if there are witnesses, then the player will also be in that table.
     -- If one of these conditions are met, then we can continue.
     if witnesses and not isPlayerAWitness(witnesses) then return end
-    if not isPlayerWhitelisted or Config.Debug then
-        local vehicle = GetVehiclePedIsUsing(ped, true)
-        if vehicle ~= 0 then
-            if vehicleWhitelist[GetVehicleClass(vehicle)] then
-                local driver = GetPedInVehicleSeat(vehicle, -1)
-                if Config.Timer['Shooting'] == 0 and not BlacklistedWeapon(ped) and not IsPedCurrentWeaponSilenced(ped) and IsPedArmed(ped, 4) then
-                    if IsPedShooting(ped) then
-                        local vehicle = vehicleData(vehicle)
-                        exports['ps-dispatch']:VehicleShooting(vehicle)
-                        Config.Timer['Shooting'] = Config.Shooting.Success
-                    else
-                        Config.Timer['Shooting'] = Config.Shooting.Fail
-                    end
-                end
-            end
-        else
-            if Config.Timer['Shooting'] == 0  and not IsPedCurrentWeaponSilenced(ped) and IsPedArmed(ped, 4) then
-                if IsPedShooting(ped) and not BlacklistedWeapon(ped) then
-                    exports['ps-dispatch']:Shooting()
+    -- If the player is a whitelisted job, then we don't want to trigger the event.
+    -- However, if the player is not whitelisted or Debug mode is true, then we want to trigger the event.
+    if IsPoliceJob(PlayerJob.name) and not Config.Debug then return end
+    local vehicle = GetVehiclePedIsUsing(ped, true)
+    if vehicle ~= 0 then
+        if vehicleWhitelist[GetVehicleClass(vehicle)] then
+            local driver = GetPedInVehicleSeat(vehicle, -1)
+            if Config.Timer['Shooting'] == 0 and not BlacklistedWeapon(ped) and not IsPedCurrentWeaponSilenced(ped) and IsPedArmed(ped, 4) then
+                if IsPedShooting(ped) then
+                    local vehicle = vehicleData(vehicle)
+                    exports['ps-dispatch']:VehicleShooting(vehicle)
                     Config.Timer['Shooting'] = Config.Shooting.Success
                 else
                     Config.Timer['Shooting'] = Config.Shooting.Fail
                 end
+            end
+        end
+    else
+        if Config.Timer['Shooting'] == 0  and not IsPedCurrentWeaponSilenced(ped) and IsPedArmed(ped, 4) then
+            if IsPedShooting(ped) and not BlacklistedWeapon(ped) then
+                exports['ps-dispatch']:Shooting()
+                Config.Timer['Shooting'] = Config.Shooting.Success
+            else
+                Config.Timer['Shooting'] = Config.Shooting.Fail
             end
         end
     end
@@ -54,37 +55,39 @@ end)
 ---@param y number | Y coord of ped
 ---@param z number | Z coord of ped
 AddEventHandler('CEventShockingMadDriver', function(witnesses, ped, x, y, z)
-    -- The ped that shot the gun must be the player.
+    -- The ped that triggered the event must be the player.
     if PlayerPedId() ~= ped then return end
-    if not isPlayerWhitelisted or Config.Debug then
-        local vehicle = GetVehiclePedIsUsing(ped, true)
-        local driver = GetPedInVehicleSeat(vehicle, -1)
-        if vehicleWhitelist[GetVehicleClass(vehicle)] then
-            if Config.Timer['Speeding'] == 0 and ped == driver then
-                if (GetEntitySpeed(vehicle) * 3.6) >= (120 + (math.random(30,60))) then
-                    Wait(400)
-                    if IsPedInAnyVehicle(ped, true) and ((GetEntitySpeed(vehicle) * 3.6) >= 90) then
-                        vehicle = vehicleData(vehicle)
-                        exports['ps-dispatch']:SpeedingVehicle(vehicle)
-                        Config.Timer['Speeding'] = Config.Speeding.Success
-                    end
-                else
-                    Config.Timer['Speeding'] = Config.Speeding.Fail
+    -- If the player is a whitelisted job, then we don't want to trigger the event.
+    -- However, if the player is not whitelisted or Debug mode is true, then we want to trigger the event.
+    if IsPoliceJob(PlayerJob.name) and not Config.Debug then return end
+    local vehicle = GetVehiclePedIsUsing(ped, true)
+    local driver = GetPedInVehicleSeat(vehicle, -1)
+    if vehicleWhitelist[GetVehicleClass(vehicle)] then
+        if Config.Timer['Speeding'] == 0 and ped == driver then
+            if (GetEntitySpeed(vehicle) * 3.6) >= (120 + (math.random(30,60))) then
+                Wait(400)
+                if IsPedInAnyVehicle(ped, true) and ((GetEntitySpeed(vehicle) * 3.6) >= 90) then
+                    vehicle = vehicleData(vehicle)
+                    exports['ps-dispatch']:SpeedingVehicle(vehicle)
+                    Config.Timer['Speeding'] = Config.Speeding.Success
                 end
+            else
+                Config.Timer['Speeding'] = Config.Speeding.Fail
             end
         end
     end
 end)
 
----@param witnesses table | Array of peds that witnessed the event, where witness[1] is the victim
+---@param witnesses table | Array of peds that witnessed the event, where witnesses[1] is the victim
 ---@param attacker number | The ped that attacked the victim
 AddEventHandler('CEventMeleeAction', function(witnesses, attacker)
-	-- The ped that shot the gun must be the player.
+	-- The ped that melee attacked must be the player.
     if PlayerPedId() ~= attacker then return end
-    if not isPlayerWhitelisted or Config.Debug then
-        if Config.Timer['Melee'] == 0 then
-            exports['ps-dispatch']:Fight()
-            Config.Timer['Melee'] = Config.Melee.Success
-        end
+    -- If the player is a whitelisted job, then we don't want to trigger the event.
+    -- However, if the player is not whitelisted or Debug mode is true, then we want to trigger the event.
+    if IsPoliceJob(PlayerJob.name) and not Config.Debug then return end
+    if Config.Timer['Melee'] == 0 then
+        exports['ps-dispatch']:Fight()
+        Config.Timer['Melee'] = Config.Melee.Success
     end
 end)
