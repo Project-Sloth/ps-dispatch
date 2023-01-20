e21b3e132c16c3c291e468a38fbd65ad284b04df
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2023-01-21 01:53:49 +1100

    Ready for PR

--- client/cl_eventhandlers.lua
+++ client/cl_eventhandlers.lua
@@ -21,9 +21,6 @@ AddEventHandler("CEventGunShot", function(witnesses, ped)
     -- If one of these conditions are met, then we can continue.
     if witnesses and not isPlayerAWitness(witnesses) then return end
     if not isPlayerWhitelisted or Config.Debug then
-        for k, v in pairs(Config.Timer) do
-            if v > 0 then Config.Timer[k] = v - 1 end
-        end
         local vehicle = GetVehiclePedIsUsing(ped, true)
         if vehicle ~= 0 then
             if vehicleWhitelist[GetVehicleClass(vehicle)] then
@@ -56,56 +53,24 @@ end)
 ---@param x number | X coord of ped
 ---@param y number | Y coord of ped
 ---@param z number | Z coord of ped
-AddEventHandler('CEventShockingMadDriver', function(ped, witnesses, x, y, z)
+AddEventHandler('CEventShockingMadDriver', function(witnesses, ped, x, y, z)
     -- The ped that shot the gun must be the player.
     if PlayerPedId() ~= ped then return end
-    if witnesses and not isPlayerAWitness(witnesses) then return end
     if not isPlayerWhitelisted or Config.Debug then
-        for k, v in pairs(Config.Timer) do
-            if v > 0 then Config.Timer[k] = v - 1 end
-        end
         local vehicle = GetVehiclePedIsUsing(ped, true)
         local driver = GetPedInVehicleSeat(vehicle, -1)
         if vehicleWhitelist[GetVehicleClass(vehicle)] then
-            if (GetEntitySpeed(vehicle) * 3.6) >= (120 + (math.random(30,60))) then
-                Wait(400)
-                if IsPedInAnyVehicle(ped, true) and ((GetEntitySpeed(vehicle) * 3.6) >= 90) then
-                    local vehicle = vehicleData(vehicle)
-                    exports['ps-dispatch']:SpeedingVehicle(vehicle)
-                    Config.Timer['Speeding'] = Config.Speeding.Success
+            if Config.Timer['Speeding'] == 0 and ped == driver then
+                if (GetEntitySpeed(vehicle) * 3.6) >= (120 + (math.random(30,60))) then
+                    Wait(400)
+                    if IsPedInAnyVehicle(ped, true) and ((GetEntitySpeed(vehicle) * 3.6) >= 90) then
+                        vehicle = vehicleData(vehicle)
+                        exports['ps-dispatch']:SpeedingVehicle(vehicle)
+                        Config.Timer['Speeding'] = Config.Speeding.Success
+                    end
+                else
+                    Config.Timer['Speeding'] = Config.Speeding.Fail
                 end
-            else
-                Config.Timer['Speeding'] = Config.Speeding.Fail
-            end
-        end
-    end
-end)
-
----@param witnesses table | Array of entity handles who witnessed the event
----@param ped number | Entity handle of the ped who triggered the event
----@param x number | X coord of ped
----@param y number | Y coord of ped
----@param z number | Z coord of ped
-AddEventHandler('CEventShockingMadDriverExtreme', function(ped, witnesses, x, y, z)
-    -- The ped that shot the gun must be the player.
-    if PlayerPedId() ~= ped then return end
-    if witnesses and not isPlayerAWitness(witnesses) then return end
-    if not isPlayerWhitelisted or Config.Debug then
-        for k, v in pairs(Config.Timer) do
-            if v > 0 then Config.Timer[k] = v - 1 end
-        end
-        local vehicle = GetVehiclePedIsUsing(ped, true)
-        local driver = GetPedInVehicleSeat(vehicle, -1)
-        if vehicleWhitelist[GetVehicleClass(vehicle)] then
-            if (GetEntitySpeed(vehicle) * 3.6) >= (120 + (math.random(30,60))) then
-                Wait(400)
-                if IsPedInAnyVehicle(ped, true) and ((GetEntitySpeed(vehicle) * 3.6) >= 90) then
-                    local vehicle = vehicleData(vehicle)
-                    exports['ps-dispatch']:SpeedingVehicle(vehicle)
-                    Config.Timer['Speeding'] = Config.Speeding.Success
-                end
-            else
-                Config.Timer['Speeding'] = Config.Speeding.Fail
             end
         end
     end
@@ -115,12 +80,8 @@ end)
 ---@param attacker number | The ped that attacked the victim
 AddEventHandler('CEventMeleeAction', function(witnesses, attacker)
 	-- The ped that shot the gun must be the player.
-    if PlayerPedId() ~= ped then return end
-    if witnesses and not isPlayerAWitness(witnesses) then return end
+    if PlayerPedId() ~= attacker then return end
     if not isPlayerWhitelisted or Config.Debug then
-        for k, v in pairs(Config.Timer) do
-            if v > 0 then Config.Timer[k] = v - 1 end
-        end
         if Config.Timer['Melee'] == 0 then
             exports['ps-dispatch']:Fight()
             Config.Timer['Melee'] = Config.Melee.Success
