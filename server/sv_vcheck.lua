ba074d9a7e25b812acd943dbdfbde4ced2a13d05
rook / German Dream RP <127600395+rookgd@users.noreply.github.com>
2023-03-29 00:10:26 +0200

    Version check
    Added version check to see if user's version is the latest available

---
+++ server/sv_vcheck.lua
@@ -0,0 +1,38 @@
+-- Returns the current version set in fxmanifest.lua
+function GetCurrentVersion()
+	return GetResourceMetadata( GetCurrentResourceName(), "version" )
+end
+
+-- Grabs the latest version number from the web GitHub
+PerformHttpRequest( "https://github.com/Project-Sloth/ps-dispatch/version.txt", function( err, text, headers )
+	-- Wait to reduce spam
+	Citizen.Wait( 2000 )
+
+	-- Print the branding!
+	print( label )
+
+	-- Get the current resource version
+	local curVer = GetCurrentVersion()
+
+	print( "  ||    Current version: " .. curVer )
+
+	if ( text ~= nil ) then
+		-- Print latest version
+		print( "  ||    Latest recommended version: " .. text .."\n  ||" )
+
+		-- If the versions are different, print it out
+		if ( text ~= curVer ) then
+			print( "  ||    ^1Project Sloth Dispatch version is outdated, visit the GitHub repository to download the newest release.\n^0  \\\\\n" )
+		else
+			print( "  ||    ^2Project Sloth Dispatch is up to date!\n^0  ||\n  \\\\\n" )
+		end
+	else
+		-- In case the version can not be requested, print out an error message
+		print( "  ||    ^1There was an error getting the latest version information.\n^0  ||\n  \\\\\n" )
+	end
+
+	-- Warn the console if the resource has been renamed, as this will cause issues with the resource's functionality.
+	if ( GetCurrentResourceName() ~= "ps-dispatch" ) then
+		print( "^1ERROR: Resource name is not ps-dispatch, expect there to be issues with the resource. To ensure there are no issues, please leave the resource name as ps-dispatch^0\n\n" )
+	end
+end )
