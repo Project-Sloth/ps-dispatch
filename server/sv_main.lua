057f541e4113e151a9d555d7f8f6c2ba4c38dd29
Jacob Bernard Allen <danjacob1233@gmail.com>
2023-05-13 22:11:00 -0400

    Attached unit fix

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -44,9 +44,9 @@ AddEventHandler("dispatch:addUnit", function(callid, player, cb)
         end
 
         if Config.AuthorizedJobs.LEO.Check(Player.PlayerData) then
-            calls[callid]['units'][#calls[callid]['units']+1] = { cid = player.cid, fullname = player.fullname, job = 'Police', callsign = player.callsign }
+            calls[callid]['units'][#calls[callid]['units']+1] = { cid = player.cid, fullname = player.fullname, job = player.job.name, callsign = player.callsign, channel = player.radio}
         elseif Config.AuthorizedJobs.EMS.Check(Player.PlayerData) then
-            calls[callid]['units'][#calls[callid]['units']+1] = { cid = player.cid, fullname = player.fullname, job = 'EMS', callsign = player.callsign }
+            calls[callid]['units'][#calls[callid]['units']+1] = { cid = player.cid, fullname = player.fullname, job = player.job.name, callsign = player.callsign, channel = player.radio}
         end
         cb(#calls[callid]['units'])
     end
