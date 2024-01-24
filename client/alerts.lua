68412d778da31afce7d9a1865bc83f1f76da5575
MonkeyWhisper <82112471+MonkeyWhisper@users.noreply.github.com>
2024-01-24 11:41:35 -0500

    Update alerts.lua

--- client/alerts.lua
+++ client/alerts.lua
@@ -6,34 +6,34 @@ local function CustomAlert(data)
 
 
     local dispatchData = {
-        message = data.message or "", -- title of the alert
-        codeName = data.dispatchCode or "NONE", -- unique name for each alert
-        code = data.code or '10-80', -- code that is displayed before the title
-        icon = data.icon or 'fas fa-question', -- icon that is displaed after the title
-        priority = data.priority or 2, -- changes color of the alert ( 1 = red, 2 = default )
-        coords = coords, -- coords of the player
-        gender = gender, -- gender of the player
-        street = GetStreetAndZone(coords), -- street of the player
-        camId = data.camId or nil, -- camId ( for heists )
-        color = data.firstColor or nil, -- color of the vehicle
-        callsign = data.callsign or nil, -- callsign of a officer/ems
-        name = data.name or nil, -- name of either officer/ems or a player
-        vehicle = data.model or nil, -- vehicle name
-        plate = data.plate or nil, -- vehicle plate
-        alertTime = data.alertTime or nil, -- time how long it stays on the screen
-        doorCount = data.doorCount or nil, -- count of the vehicle doors
-        automaticGunfire = data.automaticGunfire or false, -- is the gun automatic or not
+        message = data.message or "", -- Title of the alert
+        codeName = data.dispatchCode or "NONE", -- Unique name for each alert
+        code = data.code or '10-80', -- Code that is displayed before the title
+        icon = data.icon or 'fas fa-question', -- Icon that is displaed after the title
+        priority = data.priority or 2, -- Changes color of the alert ( 1 = red, 2 = default )
+        coords = coords, -- Coords of the player
+        gender = gender, -- Gender of the player
+        street = GetStreetAndZone(coords), -- Street of the player
+        camId = data.camId or nil, -- Cam ID ( for heists )
+        color = data.firstColor or nil, -- Color of the vehicle
+        callsign = data.callsign or nil, -- Callsigns
+        name = data.name or nil, -- Name of either officer/ems or a player
+        vehicle = data.model or nil, -- Vehicle name
+        plate = data.plate or nil, -- Vehicle plate
+        alertTime = data.alertTime or nil, -- How long it stays on the screen in seconds
+        doorCount = data.doorCount or nil, -- How many doors on vehicle
+        automaticGunfire = data.automaticGunfire or false, -- Automatic Gun or not
         alert = {
-            radius = data.radius or 0, -- radius around the blip
+            radius = data.radius or 0, -- Radius around the blip
             recipientList = job, -- job
-            sprite = data.sprite or 1, -- sprite of the blip
-            color = data.color or 1, -- color of the blip
-            scale = data.scale or 0.5, -- scale of the blip
-            length = data.length or 2, -- lenth how long it stays on the map
-            sound = data.sound or "Lose_1st", -- alert sound
-            sound2 = data.sound2 or "GTAO_FM_Events_Soundset", -- alert sound
-            offset = data.offset or "false", -- blip / radius offset
-            flash = data.flash or "false" -- blip flash
+            sprite = data.sprite or 1, -- Sprite of the blip
+            color = data.color or 1, -- Color of the blip
+            scale = data.scale or 0.5, -- Scale of the blip
+            length = data.length or 2, -- How long it stays on the map
+            sound = data.sound or "Lose_1st", -- Alert sound
+            sound2 = data.sound2 or "GTAO_FM_Events_Soundset", -- Alert sound
+            offset = data.offset or "false", -- Blip / radius offset
+            flash = data.flash or "false" -- Blip flash
         },
         jobs = { 'leo' },
     }
